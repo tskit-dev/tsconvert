68c4333e2301b8fac964594191b492671d88e056
Jerome Kelleher <jk@well.ox.ac.uk>
2019-01-17 14:51:40 +0000

    Initial Python package infrastructure.

---
+++ setup.py
@@ -0,0 +1,44 @@
+import os
+import codecs
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+with codecs.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='tsconvert',
+    description='Tree sequence conversion utilities',
+    long_description=long_description,
+    url='https://github.com/tskit-dev/tsconvert',
+    author='tskit developers',
+    # TODO setup a tskit developers email address.
+    author_email='jerome.kelleher@well.ox.ac.uk',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    keywords='tree sequence, newick, Nexus',
+    packages=['tsconvert'],
+    include_package_data=True,
+    install_requires=["tskit"],
+    entry_points={
+        'console_scripts': [
+            'tsconvert=tsconvert.__main__:main',
+        ],
+    },
+    project_urls={
+        'Bug Reports': 'https://github.com/tsconvert-dev/tsconvert/issues',
+        'Source': 'https://github.com/tsconvert-dev/tsconvert',
+    },
+    license="MIT",
+    platforms=["POSIX", "Windows", "MacOS X"],
+    setup_requires=['setuptools_scm'],
+    use_scm_version={"write_to": "tsconvert/_version.py"},
+)
---
+++ tsconvert/__main__.py
@@ -0,0 +1,7 @@
+
+def main():
+    print("NOT IMPLEMENTED")
+
+
+if __name__ == "__main__":
+    main()
